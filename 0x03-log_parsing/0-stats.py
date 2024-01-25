964583f189ea45c255bbae90452ddbf9fcc83e5c
Nawaritta <Nor.benhaddou@gmail.com>
2024-01-25 19:17:21 +0000

    update the size

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -36,15 +36,18 @@ if __name__ == "__main__":
     size = 0
     status_codes = {}
     count = 0
+    possible_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 
     try:
         for line in sys.stdin:
-            size += parse_line(line, status_codes)
+
             if count == 10:
                 print_stats(size, status_codes)
                 count = 1
             else:
                 count += 1
+            size += parse_line(line, status_codes)
+
         print_stats(size, status_codes)
 
     except KeyboardInterrupt:
