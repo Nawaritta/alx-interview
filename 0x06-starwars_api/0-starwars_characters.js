1a62183ca2e08322af16ef4364c14fac1b279956
Nawaritta <Nor.benhaddou@gmail.com>
2024-02-15 07:21:08 -0800

    semistandard fixed

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,29 +3,32 @@
 const axios = require('axios');
 const process = require('process');
 
-function getMovieCharacters(movieId) {
-    const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+function getMovieCharacters (movieId) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-    axios.get(url)
-        .then(response => {
-            const characters = response.data.characters;
-            const characterPromises = characters.map(characterUrl => {
-                return axios.get(characterUrl)
-                    .then(response => response.data.name)
-                    .catch(error => `Not found`);
-            });
-
-            return Promise.all(characterPromises);
-        })
-        .then(characterNames => {
-            characterNames.forEach(name => {
-                console.log(name);
-            });
-        })
-        .catch(error => {
+  axios.get(url)
+    .then(response => {
+      const characters = response.data.characters;
+      const characterPromises = characters.map(characterUrl => {
+        return axios.get(characterUrl)
+          .then(response => response.data.name)
+          .catch(error => {
             console.log(`Error: ${error.message}`);
             process.exit(1);
-        });
+          });
+      });
+
+      return Promise.all(characterPromises);
+    })
+    .then(characterNames => {
+      characterNames.forEach(name => {
+        console.log(name);
+      });
+    })
+    .catch(error => {
+      console.log(`Error: ${error.message}`);
+      process.exit(1);
+    });
 }
 
 const movieId = process.argv[2];
