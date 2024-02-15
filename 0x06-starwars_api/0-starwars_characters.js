82e1159ebcf2b0996114a525db83fd4ad387a2d9
Nawaritta <Nor.benhaddou@gmail.com>
2024-02-15 07:32:32 -0800

    use request module

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,34 +1,49 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 const process = require('process');
 
 function getMovieCharacters (movieId) {
   const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
 
-  axios.get(url)
-    .then(response => {
-      const characters = response.data.characters;
-      const characterPromises = characters.map(characterUrl => {
-        return axios.get(characterUrl)
-          .then(response => response.data.name)
-          .catch(error => {
-            console.log(`Error: ${error.message}`);
-            process.exit(1);
-          });
-      });
-
-      return Promise.all(characterPromises);
-    })
-    .then(characterNames => {
-      characterNames.forEach(name => {
-        console.log(name);
-      });
-    })
-    .catch(error => {
-      console.log(`Error: ${error.message}`);
+  request.get(url, (error, response, body) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
       process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+      console.error(`Error: code status ${response.statusCode}`);
+      process.exit(1);
+    }
+
+    const filmData = JSON.parse(body);
+    const characterUrls = filmData.characters;
+
+    const characterPromises = characterUrls.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request.get(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(new Error(`Character not found: ${error.message}`));
+          } else {
+            const characterData = JSON.parse(body);
+            resolve(characterData.name);
+          }
+        });
+      });
     });
+
+    Promise.all(characterPromises)
+      .then(characterNames => {
+        characterNames.forEach(name => {
+          console.log(name);
+        });
+      })
+      .catch(error => {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+      });
+  });
 }
 
 const movieId = process.argv[2];
