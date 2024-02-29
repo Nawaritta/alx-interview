22e14ae3b1790503071060a4c84dd9c0eaa13839
Nawaritta <nor.benhaddou@gmail.com>
2024-02-29 13:51:58 +0000

    simple case checked

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""This module contains makeChange(coins, total) function"""
+
+def makeChange(coins, total):
+    """Returns the minimum number of coins to meet a given amount total"""
+
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    # coef = [0] * len(coins)
+    change = 0
+    count = 0
+    i = 0
+    while change < total and i < len(coins):
+        if coins[i] <= total - change:
+            change += coins[i]
+            count += 1
+        else:
+            i += 1
+
+    if change == total:
+        return count
+    return -1
+
+
