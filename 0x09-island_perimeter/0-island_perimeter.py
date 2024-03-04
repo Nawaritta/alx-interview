0bbfb7fef8031b76add7280c06f6099ff77d343a
Nawaritta <nor.benhaddou@gmail.com>
2024-03-04 16:41:29 +0000

    Island Perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" 5. Island Perimeter """
+
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid
+    """
+    perim = 0
+    if not grid or len(grid) == 0:
+        return perim
+
+    width = len(grid)
+    height = len(grid[0])
+
+    if width > 100 or height > 100:
+        return
+
+    for i in range(width):
+        for j in range(height):
+            if grid[i][j] == 1:
+                if j + 1 >= height or (j + 1 < height and grid[i][j + 1] == 0):
+                    perim += 1
+                if j - 1 < 0 or (j - 1 >= 0 and grid[i][j - 1] == 0):
+                    perim += 1
+                if i + 1 >= width or (i + 1 < width and grid[i + 1][j] == 0):
+                    perim += 1
+                if i - 1 < 0 or (i - 1 >= 0 and grid[i - 1][j] == 0):
+                    perim += 1
+
+    return perim
