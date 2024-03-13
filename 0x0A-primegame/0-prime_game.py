77048fb4d836504a0e06e5c2f547d852d069faf4
Nawaritta <Nor.benhaddou@gmail.com>
2024-03-13 16:41:23 +0000

    is prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""This module contains isWinner function"""
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    elif number <= 3:
+        return True
+    elif number % 2 == 0 or number % 3 == 0:
+        return False
+    i = 5
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def isWinner(x, nums):
+    """ determines the winner of a game of prime numbers."""
+
+    nums.sort()
+    cursor = 0
+    count = 0
+    score = {"Maria": 0, "Ben": 0}
+
+    for i in range(1, nums[-1] + 1):
+        if is_prime(i):
+            count += 1
+        if i == nums[cursor]:
+            if count % 2 == 0:
+                score["Ben"] += 1
+            else:
+                score["Maria"] += 1
+            cursor += 1
+
+    if score["Maria"] > score["Ben"]:
+        return "Maria"
+    if score["Maria"] == score["Ben"]:
+        return None
+    return "Ben"
---
+++ 0x0A-primegame/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+isWinner = __import__('0-prime_game').isWinner
+
+
+print("Winner: {}".format(isWinner(6, [2, 2, 2, 4, 2, 2])))
